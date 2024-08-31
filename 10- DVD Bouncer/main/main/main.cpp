be8e323d18b2d17677048004e379905b2d7a06c9
Matin Afzal <contact.matin@yahoo.com>
2024-09-01 01:58:29 +0330

    10- DVD Bouncer

---
+++ 10- DVD Bouncer/main/main/main.cpp
@@ -0,0 +1,73 @@
+﻿// main.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+void updateDVD(void);
+void colorUpdate(void);
+
+sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML 2.6 / DVD Bouncer");
+sf::Event event;
+sf::Texture texture;
+sf::Sprite dvd;
+int speedX = 1.5, speedY = 1.5;
+int R = 100, G = 150, B = 170;
+
+int main()
+{
+	window.setFramerateLimit(120);
+
+	if (!texture.loadFromFile(DVD_PATH)) {
+		cerr << "DVD Image not found!" << endl;
+		return -1;
+	}
+
+	dvd.setTexture(texture);
+	dvd.setScale(sf::Vector2f(0.2, 0.2));
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event))
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		window.clear(sf::Color::Black);
+		updateDVD();
+		window.display();
+	}
+
+	return 0;
+}
+
+//*****************************************************************************
+void updateDVD(void) {
+	float dvdX = dvd.getPosition().x;
+	float dvdY = dvd.getPosition().y;
+	sf::Color color(R, G, B);
+
+	dvdX += speedX;
+	dvdY += speedY;
+
+	float dvdRealPosX = dvd.getLocalBounds().getSize().x * 0.2;
+	float dvdRealPosY = dvd.getLocalBounds().getSize().y * 0.2;
+
+	if (dvdX + dvdRealPosX >= SCREEN_WIDTH || dvdX + dvdRealPosX <= dvdRealPosX) {
+		speedX *= -1;
+		colorUpdate();
+	}
+	if (dvdY + dvdRealPosY >= SCREEN_HEIGHT || dvdY + dvdRealPosY <= dvdRealPosY) {
+		speedY *= -1;
+		colorUpdate();
+	}
+
+	dvd.setPosition(sf::Vector2f(dvdX, dvdY));
+	dvd.setColor(color);
+	window.draw(dvd);
+}
+
+void colorUpdate(void) {
+	R = rand() / 150;
+	G = rand() / 150;
+	B = rand() / 150;
+}
---
+++ 10- DVD Bouncer/main/main/main.h
@@ -0,0 +1,13 @@
+﻿// main.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define DVD_PATH "C:/Users/pc/Desktop/Desktop/Projects/CPP_Until_I_lose_My_Mind/10- DVD Bouncer/main/dvd.png"
+
+// TODO: Reference additional headers your program requires here.
